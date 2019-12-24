69888a533f339c21b164b8ec68cc51349f2232a6
gregwhitaker <gwhitake@gmail.com>
2019-12-23 17:30:09 -0800

    Doing work

--- src/main/java/parser/sec/gaaprss/model/Feed.java
+++ src/main/java/parser/sec/gaaprss/model/Feed.java
@@ -1,4 +1,71 @@
 package parser.sec.gaaprss.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class Feed {
+
+    private String title;
+    private String link;
+    private String description;
+    private String language;
+    private Date publishDate;
+    private Date lastBuildDate;
+    private List<Item> items;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public Date getLastBuildDate() {
+        return lastBuildDate;
+    }
+
+    public void setLastBuildDate(Date lastBuildDate) {
+        this.lastBuildDate = lastBuildDate;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 }
--- src/main/java/parser/sec/gaaprss/model/Item.java
+++ src/main/java/parser/sec/gaaprss/model/Item.java
@@ -1,4 +1,4 @@
 package parser.sec.gaaprss.model;
 
-public class Filing {
+public class Item {
 }
